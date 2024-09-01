f51cd0686498caa7ce8415479cf984488afc02a4
Elmoz3gg <162893646+Elmoz3gg@users.noreply.github.com>
2024-09-01 05:17:20 +0300

    Add files via upload

---
+++ plugins/MenuBot.js
@@ -0,0 +1,122 @@
+import fetch from 'node-fetch';
+
+// Para configurar o idioma, na raiz do projeto altere o arquivo config.json
+// Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
+// To set the language, in the root of the project, modify the config.json file.
+
+const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
+
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  try {
+    const datas = global
+    const idioma = datas.db.data.users[m.sender].language
+    const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+    const tradutor = _translate.plugins.menu_menu
+    // const pp = imagen7;
+
+    // let vn = './src/assets/audio/01J673Y3TGCFF1D548242AX68Q.mp3'
+    const img = './src/assets/images/menu/languages/es/menu.png';
+    const d = new Date(new Date + 3600000);
+    const locale = 'es-ES';
+    const week = d.toLocaleDateString(locale, { weekday: 'long' });
+    const date = d.toLocaleDateString(locale, { day: '2-digit', month: '2-digit', year: 'numeric' });
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const { money, joincount } = global.db.data.users[m.sender];
+    const { exp, limit, level, role } = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const rtotal = Object.entries(global.db.data.users).length || '0'
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*
+*␥↞مـࢪحـبـا بـك/ي*
+*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*
+> 🧭␥﴿ إن وعد الله حق ﴾
+*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*
+*␥↞﴿ مـعـلـومـاتـك ﴾⤹*
+> 🪔␥بـريـمـيـوم↞﴿ ${user.premiumTime > 0 ? 'مــمـ🔱ـيز' : (isPrems ? 'مــمـ🔱ـيز' : 'عــ🍁ــادي') || ''} ﴾
+> ⚜️␥مـــســـتواك↞﴿ ${level} ﴾
+> 💫␥رتـبـتـك↞﴿ ${role} ﴾
+*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*
+*␥↞﴿الـبـوت﴾⤹*
+> 🧧␥اسـم الـبـوت↞﴿ 𝐵𝑜𝑦𝑘𝑎 𝑏𝑜𝑡 ﴾
+> ❄️␥الـمـطـور↞﴿ 𝐇𝐚𝐫𝐥𝐞𝐲 𝐥𝐢𝐠𝐡𝐭 ﴾
+> ⏳␥مـده الـتـشـغـيـل↞﴿ ${uptime} ﴾
+> 💧␥عـدد الـمـسـتـخـدمـين↞﴿ 20 ﴾
+*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*
+❄️❯ ⏐ قسم الاعضاء
+§ ⌟ق1⌜
+❄️❯ ⏐ قسم الاسلام
+§ ⌟ق2⌜
+❄️❯ ⏐ قسم المطور
+§ ⌟ق3⌜
+❄️❯ ⏐ قسم التنزيلات
+§ ⌟ق4⌜
+❄️❯ ⏐ قسم الالعاب
+§ ⌟ق5⌜
+❄️❯ ⏐ قسم التحويلات
+§ ⌟ق6⌜
+❄️❯ ⏐ قـسـم الذكاء الاصطناعي
+§ ⌟ق7⌜
+❄️❯ ⏐ قسم الدعم
+§ ⌟ق8⌜
+❄️❯ ⏐ قسم المشرفين
+§ ⌟ق9⌜
+❄️❯ ⏐ قسم البحث
+§ ⌟ق10⌜
+*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*
+*قناة تحديثات البوت ✅*
+https://whatsapp.com/channel/0029ValRCUIFi8xmK9xBOr0I
+*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*
+> © 𝐇𝐚𝐫𝐥𝐞𝐲 𝐥𝐢𝐠𝐡𝐭 2024
+*⎔⋅ ╼╃ ⊰ •﹝❄️﹞• ⊱ ╄╾ ⋅⎔*`
+
+    let pp
+    // Nouvelles images de menu disponibles 
+    if (idioma == 'es') {
+      pp = global.imagen1
+    } else if (idioma == 'pt-br') {
+      pp = global.imagen2
+    } else if (idioma == 'fr') {
+      pp = global.imagen3
+    }else if (idioma == 'en') {
+      pp = global.imagen4
+    } else if (idioma == 'ru') {
+      pp = global.imagen5
+    } else {
+      pp = global.imagen1 // Imagem Default em espanhol
+    }
+
+
+
+    if (m.isGroup) {
+      // await conn.sendFile(m.chat, vn, './src/assets/audio/01J673Y3TGCFF1D548242AX68Q.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: m });
+    } else {
+      //await conn.sendFile(m.chat, vn, './src/assets/audio/01J673Y3TGCFF1D548242AX68Q.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+      conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: fkontak });
+    }
+  } catch {
+    const datas = global
+    const idioma = datas.db.data.users[m.sender].language
+    const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+    const tradutor = _translate.plugins.menu_menu
+
+    conn.reply(m.chat, tradutor.texto1[29], m);
+  }
+};
+handler.command = /^(menu|اوامر|الاوامر|مهام|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos|cmd)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+      }
