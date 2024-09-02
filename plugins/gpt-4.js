7ed8cdcc1d808250a925ef843503f0da4e29013d
Elmoz3gg <162893646+Elmoz3gg@users.noreply.github.com>
2024-09-02 08:05:19 +0300

    Update gpt-4.js

--- plugins/gpt-4.js
+++ plugins/gpt-4.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
     if (!text) throw 'uhm.. what do you want to say?';
     await m.react('🤖');
-
+    let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     const prompt = encodeURIComponent(text);
     let userid = conn.getName(m.sender) || "default"
     let apiurl = `https://api.guruapi.tech/ai/gpt4?username=${userid}&query=hii${prompt}`;
@@ -23,7 +23,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
       [['الــمــطـور', `.المطور`]], 
       null, 
       [['قــنـاة الــبــوت', `https://whatsapp.com/channel/0029ValRCUIFi8xmK9xBOr0I`]], 
-      null, null, fkontak
+      null, null, fkontak2
     );
   } catch (error) {
     console.error(error);
